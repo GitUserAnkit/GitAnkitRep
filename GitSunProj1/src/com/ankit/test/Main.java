97945def7440f6c19e4db0a300294f0d62bbeafe
ANKIT <ANKIT@DESKTOP-HL71OEO>
2018-06-10 15:39:19 -0700

    khjghjhj

--- GitSunProj1/src/com/ankit/test/Main.java
+++ GitSunProj1/src/com/ankit/test/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to git hub");
-   
+        add(10,20);
+	}
+	public static int add(int a,int b){
+		return a+b;
 	}
 
 }
