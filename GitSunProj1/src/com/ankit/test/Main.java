f9e206c00f30bdb253d6b06ab189a101beba2d1b
ANKIT <ANKIT@DESKTOP-HL71OEO>
2018-06-10 15:28:14 -0700

    modifiacation in main.java by dev1

--- GitSunProj1/src/com/ankit/test/Main.java
+++ GitSunProj1/src/com/ankit/test/Main.java
@@ -1,10 +1,13 @@
 package com.ankit.test;
 
 public class Main {
+	public void message(){
+		System.out.println("Good Morning");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to git hub");
-
+   
 	}
 
 }
